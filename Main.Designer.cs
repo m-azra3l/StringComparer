827ac64803cfeade0af18ebb24878c3503c04a7c
Michael <gadreelazazel@gmail.com>
2023-06-16 09:07:16 +0100

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace StringComparer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            progressBar1 = new ProgressBar();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(83, 76);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Main String";
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.Size = new Size(555, 368);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(739, 76);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Compared String";
+            textBox2.ScrollBars = ScrollBars.Both;
+            textBox2.Size = new Size(555, 368);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(83, 605);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Difference";
+            textBox3.ScrollBars = ScrollBars.Both;
+            textBox3.Size = new Size(1211, 213);
+            textBox3.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(83, 565);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(1211, 34);
+            progressBar1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(593, 484);
+            button1.Name = "button1";
+            button1.Size = new Size(187, 34);
+            button1.TabIndex = 4;
+            button1.Text = "Compare";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(1378, 844);
+            Controls.Add(button1);
+            Controls.Add(progressBar1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "String Comparer";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ProgressBar progressBar1;
+        private Button button1;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# StringComparer
+ String comparing desktop application to compare two sets of strings.
