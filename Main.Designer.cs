82c7b7496c3f8ed43eb616e7ae1bccd2c4dec1a5
Michael <gadreelazazel@gmail.com>
2023-06-16 11:41:43 +0100

    Update

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -111,7 +111,7 @@
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
-            BackColor = SystemColors.Desktop;
+            BackColor = SystemColors.Control;
             ClientSize = new Size(1378, 844);
             Controls.Add(textBox4);
             Controls.Add(diffLabel);
